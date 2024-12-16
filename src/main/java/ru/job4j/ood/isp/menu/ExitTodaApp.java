cdae093b13122c6f6f05d267fe383323802c0bfe
AlexDouglasstochkajpg <domnitzkij.alexander@yandex.ru>
2024-12-16 15:22:48 +0300

    1. Создать меню. [#4748]

---
+++ src/main/java/ru/job4j/ood/isp/menu/ExitTodaApp.java
@@ -0,0 +1,12 @@
+package ru.job4j.ood.isp.menu;
+
+public class ExitTodaApp implements UserAction {
+    @Override
+    public String name() {
+        return "Выход из программы";
+    }
+    @Override
+    public boolean execute(Menu menu, ActionDelegate action) {
+        return false;
+    }
+}
